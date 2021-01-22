cb84ef15fa689eb24937dea9c7dbe90f70afa463
Sujit Thonse <sujit.thonse@sap.com>
2021-01-22 14:14:46 +0100

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/myorg/myGitFlowDemo/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.myorg.myGitFlowDemo.controller.MainView", {});
+});
